0a601dc12584fdbb30448116eefa9242eefded17
lin11220206 <lin11220206@gmail.com>
2015-10-22 00:45:47 +0800

    Complete all function in client.c and server.c

--- client.c
+++ client.c
@@ -8,8 +8,13 @@
 
 #define PORT 6666
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(argc != 3) {
+        printf("Usage: %s [ip] [port]\n", argv[0]);
+        exit(1);
+    }
+    //Create client socket
     int client_socket;
     struct sockaddr_in client_addr;
     bzero(&client_addr, sizeof(client_addr));
@@ -29,12 +34,17 @@ int main()
         exit(1);
     }
 
+    //Store server information
     struct sockaddr_in server_addr;
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = PF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_addr.sin_port = htons(PORT);
+    if(inet_aton(argv[1], &server_addr.sin_addr) == 0) {
+        printf("Server IP Address Error\n");
+        exit(1);
+    }
+    server_addr.sin_port = htons(atoi(argv[2]));
 
+    //Connect to server
     if(connect(client_socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0 ) {
         printf("Connect to Server Failed\n");
         exit(1);
@@ -72,7 +82,7 @@ int main()
             bzero(buffer, 1024);
             strcpy(buffer, "c ");
             strcat(buffer, fileName);
-            send(client_socket, buffer, strlen(buffer), 0);
+            send(client_socket, buffer, 1024, 0);
 
             //receive message
             bzero(buffer, 1024);
@@ -90,7 +100,7 @@ int main()
             bzero(buffer, 1024);
             strcpy(buffer, "r ");
             strcat(buffer, fileName);
-            send(client_socket, buffer, strlen(buffer), 0);
+            send(client_socket, buffer, 1024, 0);
 
             bzero(buffer, 1024);
             if( recv(client_socket, buffer, 1024, 0) < 0)
@@ -106,13 +116,15 @@ int main()
         } else if(strcmp(inst, "list") == 0) {
             bzero(buffer, 1024);
             strcpy(buffer, "l");
-            send(client_socket, buffer, strlen(buffer), 0);
+            send(client_socket, buffer, 1024, 0);
 
             bzero(buffer, 1024);
             if( recv(client_socket, buffer ,1024, 0) <0)
                 printf("Receive Message Failed\n");
             printf("File in Directory: \n");
+            //get list length
             int i, number = atoi(buffer);
+            //print list
             for(i=0; i<number; ++i) {
                 bzero(buffer, 1024);
                 recv(client_socket, buffer, 1024, 0);
@@ -140,9 +152,11 @@ int main()
                     printf("Can't Create %s In Client\n", fileName);
                     continue;
                 }
+                //get file_block_number
                 int count = atoi(buffer);
                 bzero(buffer, 1024);
                 int length = 0;
+                //save data
                 while (count--) {
                     length = recv(client_socket, buffer, 1024, 0);
                     if(length < 0) {
@@ -160,7 +174,8 @@ int main()
                 printf("Download %s Successfully\n", fileName);
             }
         } else if(strcmp(inst, "edit") == 0) {
-			bzero(buffer, 1024);
+            //download a temp file in this directory
+            bzero(buffer, 1024);
             strcpy(buffer, "d ");
             strcat(buffer, fileName);
             send(client_socket, buffer, 1024, 0);
@@ -174,9 +189,9 @@ int main()
             else if(strcmp(buffer, "error") == 0)
                 printf("Access To %s Failed\n", fileName);
             else {
-				char tmpName[25];
-				strcpy(tmpName, fileName);
-				strcat(tmpName, ".tmp");
+                char tmpName[25];
+                strcpy(tmpName, fileName);
+                strcat(tmpName, ".tmp");
                 fp = fopen(tmpName, "wb");
                 if(fp == NULL) {
                     printf("Can't Open %s\n", fileName);
@@ -199,51 +214,63 @@ int main()
                     bzero(buffer, 1024);
                 }
                 fclose(fp);
-				char inst[30];				
-				bzero(inst, 30);
-				strcpy(inst, "vi ");
-				strcat(inst, tmpName);
-				printf("System Will Use 'vi' To Edit File, Press Any Key To Continue\n");
-				fgetc(stdin);				
-				system(inst);
-				
-				fp = fopen(tmpName, "rb");
-				if(fp == NULL)
-				{
-					printf("Save File Failed\n");
-					continue;
-				}
-				int file_block_length = 0;
-				int file_block_number = 0;
-				while(fread(buffer, sizeof(char), 1024, fp) > 0)
-					file_block_number++;
-					
-				rewind(fp);
-				sprintf(buffer, "%d", file_block_number);
-				send(client_socket, buffer, 1024, 0);
-				bzero(buffer, 1024);
 
-				while((file_block_length = fread(buffer, sizeof(char), 1024, fp)) > 0)
-				{
-					if(send(client_socket, buffer, file_block_length, 0) < 0)
-					{
-						printf("Save File Failed\n");
-						break;
-					}		
-					bzero(buffer, 1024);
-				}
-				fclose(fp);
-			
-				bzero(inst, 25);
-				strcpy(inst, "rm ");
-				strcat(inst, tmpName);
-				system(inst);
-                printf("Edit %s Successfully\n", fileName);
+                //edit file with 'vi'
+                char inst[30];
+                bzero(inst, 30);
+                strcpy(inst, "vi ");
+                strcat(inst, tmpName);
+                printf("System Will Use 'vi' To Edit File, Press Any Key To Continue\n");
+                fgetc(stdin);
+                system(inst);
+
+
+                //send file back
+                strcpy(buffer, "e ");
+                strcat(buffer, fileName);
+                send(client_socket, buffer, 1024, 0);
+                bzero(buffer, 1024);
+
+                fp = fopen(tmpName, "rb");
+                if(fp == NULL) {
+                    printf("Save File Failed\n");
+                    continue;
+                }
+                int file_block_length = 0;
+                int file_block_number = 0;
+                while(fread(buffer, sizeof(char), 1024, fp) > 0)
+                    file_block_number++;
+
+                rewind(fp);
+                sprintf(buffer, "%d", file_block_number);
+                send(client_socket, buffer, 1024, 0);
+                bzero(buffer, 1024);
+
+                while((file_block_length = fread(buffer, sizeof(char), 1024, fp)) > 0) {
+                    if(send(client_socket, buffer, file_block_length, 0) < 0) {
+                        printf("Save File Failed\n");
+                        break;
+                    }
+                    bzero(buffer, 1024);
+                }
+                fclose(fp);
+
+                //remove temp file
+                bzero(inst, 25);
+                strcpy(inst, "rm ");
+                strcat(inst, tmpName);
+                system(inst);
+                recv(client_socket, buffer, 1024, 0);
+                if(strcmp(buffer,"success") == 0)
+                    printf("Edit %s Successfully\n", fileName);
+                else
+                    printf("Edit %s Failed\n", fileName);
             }
-	
+
         } else if(strcmp(inst, "quit") == 0) {
             bzero(buffer, 1024);
             strcpy(buffer, "q");
+            //send message to let server know that this client quit
             send(client_socket, buffer, strlen(buffer), 0);
 
             close(client_socket);
--- server.c
+++ server.c
@@ -7,19 +7,22 @@
 #include<dirent.h>
 #include<unistd.h>
 
-#define PORT 6666
-
 int findFile(DIR *dir, char *fileName);
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(argc != 2) {
+        printf("Usage: %s [port]\n", argv[0]);
+        exit(0);
+    }
+    //create server socket
     int server_socket;
     struct sockaddr_in server_addr;
     //bzero(&server_addr, sizeof(server_addr));
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = PF_INET;
     server_addr.sin_addr.s_addr = htons(INADDR_ANY);
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(atoi(argv[1]));
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -29,10 +32,10 @@ int main()
     }
 
     if( bind(server_socket, (struct sockaddr*) &server_addr, sizeof(server_addr))) {
-        printf("Server Bind port Failed\n");
+        printf("Server Bind Port: %s Failed\n", argv[1]);
         exit(1);
     }
-
+    //listen port
     if( listen(server_socket, 10)) {
         printf("Server Listen Failed\n");
         exit(1);
@@ -57,6 +60,7 @@ int main()
         memset(&client_addr, 0, client_addr_length);
         //bzero(client_addr, client_addr_length);
 
+        //accept client socket
         new_server_socket = accept(server_socket, (struct sockaddr*) &client_addr, &client_addr_length);
         if(new_server_socket < 0) {
             printf("Server Accept Failed\n");
@@ -68,6 +72,7 @@ int main()
         int length;
         while(1) {
             bzero(buffer, 1024);
+            //receive command from client
             if( recv(new_server_socket, buffer, 1024, 0) < 0 ) {
                 printf("Server Receive Command Failed\n");
                 continue;
@@ -82,6 +87,7 @@ int main()
             strncpy(command, buffer, length);
 
             sscanf(command, "%c %s", &option, fileName);
+            //list
             if(option == 'l') {
                 struct dirent *ptr;
                 int i=0;
@@ -99,11 +105,14 @@ int main()
                 printf("Their Are %d File In Directory\n", i);
                 free(ptr);
             }
-            //
+            //quit
             else if(option == 'q') {
                 printf("Client Quit System\n");
+                close(new_server_socket);
                 break;
-            } else if(option == 'c') {
+            }
+            //create file
+            else if(option == 'c') {
                 //file already exist
                 if( findFile(dir, fileName) != 0) {
                     strcpy(buffer, "fail");
@@ -119,7 +128,6 @@ int main()
                 strcpy(buffer, "success");
                 send(new_server_socket, buffer, 1024, 0);
                 printf("Create File Successfully\n");
-                continue;
             } else {
                 //send message 'File doesn't exist'
                 if( findFile(dir, fileName) == 0 ) {
@@ -128,6 +136,7 @@ int main()
                     printf("File doesn't Exist\n");
                     continue;
                 }
+                //remove file
                 if(option == 'r') {
                     strcpy(inst, "rm ./file/");
                     strcat(inst, fileName);
@@ -136,8 +145,9 @@ int main()
                     strcpy(buffer, "success");
                     send(new_server_socket, buffer, strlen(buffer), 0);
                     printf("Remove File successfully\n");
-                    continue;
-                } else if(option == 'd') {
+                }
+                //download file
+                else if(option == 'd') {
                     strcpy(fileName2, "./file/");
                     strcat(fileName2, fileName);
                     fp = fopen(fileName2, "rb");
@@ -149,6 +159,7 @@ int main()
                     }
                     int file_block_length = 0;
                     int file_block_number = 0;
+                    //count block number
                     while(fread(buffer, sizeof(char), 1024, fp) > 0)
                         file_block_number++;
 
@@ -156,6 +167,7 @@ int main()
                     sprintf(buffer, "%d", file_block_number);
                     send(new_server_socket, buffer, 1024, 0);
                     bzero(buffer, 1024);
+                    //send file
                     while((file_block_length = fread(buffer, sizeof(char), 1024, fp)) > 0) {
                         if(send(new_server_socket, buffer, file_block_length, 0) < 0) {
                             printf("Send File Failed\n");
@@ -165,49 +177,53 @@ int main()
                     }
                     fclose(fp);
                     printf("Transfer File Successfully\n");
-                } else if(option == 'e') {
+                }
+                //edit file
+                else if(option == 'e') {
+
+                    int check = 0;
                     strcpy(fileName2, "./file/");
                     strcat(fileName2, fileName);
-                    fp = fopen(fileName2, "rb");
-                    if(fp == NULL) {
-                        strcpy(buffer, "error");
-                        send(new_server_socket, buffer, strlen(buffer), 0);
-                        printf("Open File Failed\n");
-                        continue;
-                    }
-                    bzero(buffer, 1024);
-                    int file_block_length = 0;
-                    while((file_block_length = fread(buffer, sizeof(char), 1024, fp)) > 0) {
-                        if(send(new_server_socket, buffer, file_block_length, 0) < 0) {
-                            printf("Send File Failed\n");
-                            break;
-                        }
-                        bzero(buffer, 1024);
-                    }
-                    fclose(fp);
-                    strcpy(buffer, "success");
-                    send(new_server_socket, buffer, strlen(buffer), 0);
-                    printf("Transfer File Successfully\n");
-
                     fp = fopen(fileName2, "wb");
+                    if(fp == NULL) {
+                        printf("Access File Failed\n");
+                        check = 1;
+                    }
 
+                    if( recv(new_server_socket, buffer, 1024, 0) < 0) {
+                        printf("Receive Count Failed\n");
+                        check = 1;
+                    }
+                    //get file_block_number
+                    int count = atoi(buffer);
                     bzero(buffer, 1024);
                     int length = 0;
-                    while( length = recv(new_server_socket, buffer, 1024, 0) ) {
+                    //save data
+                    while(count--) {
+                        length = recv(new_server_socket, buffer, 1024, 0);
                         if( length < 0) {
                             printf("Receive Data From Client Failed\n");
+                            check = 1;
                             break;
                         }
 
                         int write_length = fwrite(buffer, sizeof(char), length, fp);
                         if(write_length < length) {
                             printf("Write File Failed\n");
+                            check = 1;
                             break;
                         }
                         bzero(buffer, 1024);
                     }
                     fclose(fp);
-                    printf("End Receive File\n");
+                    if(check == 0)
+                        strcpy(buffer, "success");
+                    else
+                        strcpy(buffer, "fail");
+
+                    send(new_server_socket, buffer, 1024, 0);
+                    bzero(buffer, 1024);
+                    printf("End Edit File\n");
                 } else {
                     printf("Unkonwn Error Cause Illegal Command\n");
                     continue;
@@ -215,10 +231,10 @@ int main()
             }
         }
     }
-
+    close(server_socket);
     return 0;
 }
-
+//check if file exist
 int findFile(DIR *dir, char *fileName)
 {
     dir = opendir("./file");
